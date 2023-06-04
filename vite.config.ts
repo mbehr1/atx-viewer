529f1cfbfc797acaf7e15ad2ef81b2c117b5d22e
Matthias Behr <mbehr@mcbehr.de>
2023-06-04 22:09:52 +0200

    build: add commit hash as define

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import * as child from "child_process";
+
+const commitHash = child.execSync("git rev-parse --short HEAD").toString();
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    __COMMIT_HASH__: JSON.stringify(commitHash),
+  },
   plugins: [
     react(),
     nodePolyfills({
